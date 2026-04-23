1de8f8973977f439e1019d2a4939ce630dff9a4d
Keshia Rose <keshia.rose@fingerprint.com>
2026-04-23 12:33:12 -0700

    Update rentals.js

--- personalization/server/rentals.js
+++ personalization/server/rentals.js
@@ -1,21 +1,11 @@
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import fs from "fs";
 
 const rentals = JSON.parse(
-  readFileSync(join(__dirname, "data/rentals.json"), "utf8")
+  fs.readFileSync("./server/data/rentals.json", "utf-8")
 );
 
 function shuffle(arr) {
-  const copy = [...arr];
-  for (let i = copy.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [copy[i], copy[j]] = [copy[j], copy[i]];
-  }
-  return copy;
+  return [...arr].sort(() => Math.random() - 0.5);
 }
 
 export function getRentals() {
